459c7a3212bb5116f85bb760b8d859821a5a7bbf
Fuadmikayil <mikayilov2006@gmail.com>
2025-04-20 17:53:21 +0400

    title icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,19 @@
 import Header from "./components/header";
 import "./globals.css";
-
 export const metadata = {
   title: "PapaJhons",
   description: "Pizza",
+  icons: {
+    icon: '/favicon.png', // <- bunu əlavə edirik
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-    <Header/>
+        <Header />
 
         {children}
       </body>
